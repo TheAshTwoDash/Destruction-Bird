bf775b54ef166c77d278a31e8ea4f1ef1b877268
TheAshTwoDash <theashtwodash@gmail.com>
2024-05-19 20:14:39 +0200

    update

--- building_move.gd
+++ building_move.gd
@@ -1,10 +1,10 @@
 extends StaticBody2D
 
-var dup = self.duplicate()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position = Vector2(30, 0)
-	self.duplicate().add_child(dup)
+
 	pass # Replace with function body.
 
 
